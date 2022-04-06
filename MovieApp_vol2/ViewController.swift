20daf0455d0612e5dc17d2f0b29d76a4d7d94224
Dino Smircic <dino.smircic@gmail.com>
2022-04-07 00:30:55 +0200

    Initial Commit

---
+++ MovieApp_vol2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp_vol2
+//
+//  Created by Dino Smirčić on 07.04.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
